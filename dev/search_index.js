0092f76ffdeedb603a34bf5b2e48e71d0d4ca1a0
Documenter.jl <documenter@juliadocs.github.io>
2022-09-15 06:05:35 +0000

    build based on 9a31d08

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KanekoExperimentsCesar","category":"page"},{"location":"#KanekoExperimentsCesar","page":"Home","title":"KanekoExperimentsCesar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KanekoExperimentsCesar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KanekoExperimentsCesar]","category":"page"}]
+}
